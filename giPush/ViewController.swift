5838463711a663821bf01c5f646dbf94efb93268
AlexanderLewis <alexecth@gmail.com>
2017-03-25 17:07:40 +0000

    Initial Commit

---
+++ giPush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  giPush
+//
+//  Created by Alex Lewis on 25/03/2017.
+//  Copyright © 2017 Alex Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
